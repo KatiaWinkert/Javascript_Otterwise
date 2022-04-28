2d31aadd612220bc79a15ea7b6d360b8ee6bf172
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-27 23:55:29 -0300

    Expressão logica
    Expressoes logicas - And - Or e not

---
+++ aula1/Explog.and-or-not.js
@@ -0,0 +1,24 @@
+// Expressoes logicas And(&&) Or(||) e not(!)
+
+console.log( true && false)
+
+/* Tabela verdade de And(&&) operador Binario
+ True && True = True
+ True && False = False 
+ False && True = False 
+ False && False = False */
+
+ console.log (true || false) 
+
+ /* Tabela verdade de Or(||) operador Binario 
+ True || true = True
+ True || false = true
+ False || True = True
+ False || false = False 
+ */
+
+console.log(!false )
+
+/* not (!) - Ele é unario ou seja opera somente sobre um elemento 
+ !true = False 
+ !false = True */ 
