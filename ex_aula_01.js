22c404b8bd9d51dc302d561fce701a78590b7af0
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-28 00:32:15 -0300

    ex_aula_01.js
    Atividade concluida

---
+++ ex_aula_01.js
@@ -0,0 +1,27 @@
+// Atividade aula 1 
+// letra A - imprima no console a Multiplicação de 42 por -3.14
+console.log(42*-3.14)
+
+//-----------------------------------------
+// letra B - Atribua a uma constante a soma das strings "otter" e "wise" e imprima no console seu valor 
+
+const nome =  ("Otter" + "wise")
+console.log(nome)
+
+//-----------------------------------------
+//letra C - atribua a uma variavel o resutado da operação And entre os valores booleanos true e false e imprima no console seu valor. 
+
+let a = ( true && false)
+console.log(a)
+
+//------------------------------------------
+// letra D - atribua a uma variavel o resultado da operação OR entre os valores booleanos true e false e imprima seu valro no console. 
+
+let o = (true || false )
+console.log(o)
+
+//------------------------------------------
+//letra E - imprima no console o resultado da operação NOT com o valor true 
+
+let n = (!false)
+console.log (n)
