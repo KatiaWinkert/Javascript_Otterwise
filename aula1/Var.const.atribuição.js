60105ec63f10649f67ed2ff6516621c1cc959ca9
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-28 00:17:36 -0300

    commit

--- aula1/Var.const.atribuição.js
+++ aula1/Var.const.atribuição.js
@@ -1,8 +1,10 @@
 let x  // (let variavel) (x é nome da variavel) (= recebe) 10
 x = 'oi'
 
-const y = 'teste' // constante
+const y = 'teste' // constante variavel no qual nao permite alteração de seu valor 
 console.log(x)
 
 x = 'tchau' // atibuição
 console.log(x)
+
+// let variavel que tem permição de alteração de seu valor
