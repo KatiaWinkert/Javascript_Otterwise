6923a10394fc96586a7398a84b151f24529d3288
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-29 01:11:03 -0300

    ex aula 2

---
+++ aula2/ex_aula2.js
@@ -0,0 +1,11 @@
+//1 excercicio 
+function checksignal(number) {
+  if (number >= 0) {
+    console.log(number + ' se o numero for positivo')
+  } else {
+    console.log(number + ' se o numero for negativo')
+  }
+}
+checksignal(-99)
+checksignal(42)
+
