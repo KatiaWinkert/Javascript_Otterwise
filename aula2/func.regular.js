afeb33d089fad55060d12b0c3661ad700f1e1e93
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-29 00:24:08 -0300

    commit

---
+++ aula2/func.regular.js
@@ -0,0 +1,19 @@
+const x = 10
+let y = 15
+
+function soma(a, b) {
+  return a + b
+}
+
+function multiplica(a,b){
+    return a*b
+}
+
+const result1 = soma(3, 5) // 8
+console.log(result1)
+
+const result2 = soma(-12, 42) // 30
+console.log(result2)
+
+const result3 = multiplica(x, y + 10) // 25
+console.log(result3)
