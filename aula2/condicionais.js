8451edc58ac8e3fa59ff00a5d8496d283cfe0941
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-28 23:13:23 -0300

    commit

--- aula2/condicionais.js
+++ aula2/condicionais.js
@@ -1,6 +1,11 @@
 const value = 10 
 
-if (false) {
-    console.log('consição verdadeira')
+let booli1 = true
+let booli2 = false
+
+if (booli1 || booli2 ) {
+    console.log('consição true')
+} else {
+    console.log('condição False')
 }
 console.log(value)
---
+++ switch.js
@@ -0,0 +1,24 @@
+const value = 10 
+
+switch (value) {
+    case 1:
+console.log('é 1')
+    break; 
+case 2:
+    console.log('é 2')
+    break;
+
+    default:
+        console.log('Não nenhum dos valores')
+    break;
+}
+
+if (value === 1){
+    console.log('é 1')
+} else if (value === 2 ){
+    console.log('é 2')
+} else {
+    console.log('não é nenhum dos valores acima')
+}
+
+console.log(value)
