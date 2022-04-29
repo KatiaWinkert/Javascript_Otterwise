d88a6c0cf71c764f4d20fadd6a1cc4a507998053
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-29 11:39:13 -0300

    homework concluded

--- aula2/ex_aula2.js
+++ aula2/ex_aula2.js
@@ -9,3 +9,20 @@ function checksignal(number) {
 checksignal(-99)
 checksignal(42)
 
+console.log('__________________________________________________')
+// 2 exercicio
+
+function double(number){
+  return number*2
+}
+console.log(double(5))
+
+console.log('__________________________________________________')
+
+
+
+function calculate(n1, n2) {
+  return n1 + n2
+}
+const number1 = calculate(3,5)
+console.log(`A soma é ${number1}`)
