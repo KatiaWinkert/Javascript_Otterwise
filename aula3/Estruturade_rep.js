1b74c051f76f273eb5204ab18f2ea0cc6273881b
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-04-29 21:20:01 -0300

    Create Estrutura_rep.js

---
+++ aula3/Estruturade_rep.js
@@ -0,0 +1,14 @@
+/*let array = [10,20,30]
+
+for (let i = 0; i < 3; i++){
+    console.log(array[i])
+}*/
+
+//_____________________________________while 
+
+let e = 0
+
+while (e < 3 ){
+console.log(e)
+e++
+}
