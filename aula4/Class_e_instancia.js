49e542f982e3076c463d29702034aa6c26af08c6
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-02 16:04:53 -0300

    commit

---
+++ aula4/Class_e_instancia.js
@@ -0,0 +1,13 @@
+
+
+const num = 12
+
+console.log(Number.isInteger(num))
+console.log(Number(num.toFixed(2)))
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+if(100000000000000000000000000000000000000000000000000000000000000000000000000000000 < Number.MAX_VALUE){
+    console.log('é menor')
+}
