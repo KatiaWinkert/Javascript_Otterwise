091cc2ef42eeb134bb45ed4e394605900e7f4c01
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-02 23:53:52 -0300

    Create Operador Condicional

---
+++ aula5/Op_condc.js
@@ -0,0 +1,14 @@
+// Operador Condicional: If ternario.  let x = 10
+//sintaxe : condição ? exp 1 : exp 2 => "const resp = x < 10 ? 'é menor que 10' : 'é maior ou igual a 10'
+
+const x = 10
+
+/*if(x < 10 ){
+    console.log('é menor que 10')
+} else {
+    console.log('é maior ou igual a 10')
+}*/
+
+//x < 10 ? console.log('é menor que 10') : console.log('é maior ou igual a 10')
+
+console.log(x <10 ? 'é menor que 10' : 'é maior ou igual a 10')
