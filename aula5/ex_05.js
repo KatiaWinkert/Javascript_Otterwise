4a10a3ef83ff97b8d5572008877eeb7a70c4df09
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-03 00:25:47 -0300

    commit

--- aula5/ex_05.js
+++ aula5/ex_05.js
@@ -1,7 +1,11 @@
-// 1 - Atividade
+const user = {
+  name: 'Marilia',
+  age: 26,
+  job: 'Dev',
+}
 
-const user ={
-    name: 'Marília',
-    age: 26,
-    job: 'Dev'
+const objkey = Object.entries(user)
+
+for (let i = 0; i < objkey.length; i++) {
+  console.log('o valor da propriedade', `${objkey[i].join(' é ')}`)
 }
