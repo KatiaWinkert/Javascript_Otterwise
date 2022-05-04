4748b342dfc8111bfc2130d3b87faa63c50d7928
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-03 23:11:50 -0300

    Função anônima

---
+++ aula6/função_anonima.js
@@ -0,0 +1,18 @@
+//function (firstNumber, secondNumber) { return firstNumber + secondNumber} 
+
+/*const result = (function(firstNumber, secondNumber){
+    return firstNumber + secondNumber
+})(1,2)
+console.log(result)*/
+
+/*function soma(firstNumber, secondNumber){
+    return firstNumber + secondNumber
+}
+const reslt = soma (1,2)
+console.log(reslt)*/
+
+const result = (() =>{
+    console.log('hello world')
+})()
+
+
