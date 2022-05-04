e1b0e8c90c54a514afe7d048f94bd2444bb30638
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-03 22:41:43 -0300

    commit

--- aula5/ex_05.js
+++ aula5/ex_05.js
@@ -9,3 +9,39 @@ const objkey = Object.entries(user)
 for (let i = 0; i < objkey.length; i++) {
   console.log('o valor da propriedade', `${objkey[i].join(' é ')}`)
 }
+
+console.log('______________________________________________________')
+// Atividade 2 
+const users = [
+  {
+    name: 'Marília',
+    age: 26,
+    job: 'Dev',
+  },
+  {
+    name: 'Juca',
+    age: 21,
+    job: 'RH',
+  },
+  {
+    name: 'Flávia',
+    age: 30,
+    job: 'Financeiro',
+  },
+  {
+    name: 'Sérgio',
+    age: 24,
+    job: 'Dev',
+  },
+]
+
+const vet = []
+
+for(let i = 0; i < users.length; i++){
+  vet.push(
+    `${users[i].name} ${users[i].job === 'Dev' ? 'é Dev' : 'Não é Dev.'}`
+  )
+}
+
+console.log(vet.join(' '))
+
