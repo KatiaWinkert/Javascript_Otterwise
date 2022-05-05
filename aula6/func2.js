d687b4b696b28eb45ba99520c392ca12edfd26af
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-04 22:29:49 -0300

    commit

--- aula6/func2.js
+++ aula6/func2.js
@@ -3,3 +3,13 @@ function mult(a) {
 }
 
 console.log(mult(3)(10)) // b => 3 * b -> 2 = 3 * 2 = 6
+
+/* HOF - High-Oder Function 
+é uma função que manipula outras, seja recebendo uma função como argumento ou retornando uma ex: 
+    function aplica (func, value) {
+        return func (value) >>> a função aplica esta usando o func depois do return entao é um callback function 
+    }
+
+
+                        
+*/
