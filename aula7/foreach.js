6c8f770d920ba29fcd399ad990dbb5acd5960d5e
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-05 23:19:00 -0300

    Create foreach

---
+++ aula7/foreach.js
@@ -0,0 +1,20 @@
+const array = [10,20,30,40,50]
+
+//for(let i = 0; i < array.length; i++ ){
+//    console.log(array[i])
+//}
+
+/*array.forEach (function  imprime (value) { 1° exemplo 
+  console.log(value)
+})*/
+
+/*array.forEach(function (value ){    (função anonima > sem o nome ) 2° exemplo
+    console.log(value)
+})*/
+
+/*array.forEach(value => {    // arrow function  3° Exemplo 
+  console.log(value)
+})*/ 
+
+array.forEach (value => console.log(value) // sem o retorno  4° Exemplo: 
+)
