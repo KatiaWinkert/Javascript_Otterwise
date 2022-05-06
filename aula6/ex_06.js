0bbf9102adc992b435fef1fb56f6add54061f874
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-05 22:23:33 -0300

    Concluido
    atividade finalizada.

--- aula6/ex_06.js
+++ aula6/ex_06.js
@@ -15,3 +15,22 @@ const executeOnArray = function (a , func) {
 const double = (arg) => arg * 8 
 
 executeOnArray([1,2,3,4,5], double)
+
+// exercico 3 
+
+const op = {
+    soma : (numOne, numTwo) => numOne + numTwo, 
+    subtrair : (numOne, numTwo) => numOne - numTwo,
+    multipllicação : (numOne, numTwo) => numOne * numTwo, 
+    Divisão : (numOne, numTwo) => numOne / numTwo,
+}
+
+const calculate = (arg) => {
+    return (numOne, numTwo) => op [arg](numOne,numTwo)
+}
+
+console.log(calculate('soma')(4,10))
+console.log(calculate('subtrair')(6,4))
+console.log(calculate('multipllicação')(2,2))
+console.log(calculate('Divisão')(2,10))
+
