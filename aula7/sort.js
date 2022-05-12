811f31c2c95c15b9d6ffdd5ea9512091404ebf3b
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-11 23:08:02 -0300

    commit

--- aula7/sort.js
+++ aula7/sort.js
@@ -12,13 +12,20 @@ console.log(array)
 
 console.log('__________________________________________')
 
-const nino = [-42, -100, -8.5, 20, 200 , 65.65 ]
+const nino = [-42, -100, -8.5, 20, 200 , 20 ]
 console.log(nino)
 
 const arrayNum = nino.sort((a,b) => {
-    if (a < b ) return -1               // quando vc inverte  o sinal após o return na condição ele faz a impressao de forma decrescente 
-    if (a > b) return 1
-     return 0
+    return a - b  
 })
-
 console.log(arrayNum)
+
+// a = 20 
+// b = 200 
+// a = -42 / b = -100 -> + positivo  // -42 - (-100) = 58  - 58 é um numero positivo 
+// a = 20 / b = 200 -> - negativo  // 20 - 200 = - 180  é negativo
+// a = 20 / b = 20 -> 0  // 20 -20 = 0 
+// a = -42 / b = 20 -> - negativo // - 42 - 20 = -62 negativo  
+               // quando vc inverte  o sinal após o return na condição ele faz a impressao de forma decrescente
+ 
+
