b7de95b5792056d9cb2c64dfdd6b2c46a2aea6cb
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-11 22:46:08 -0300

    commit

--- aula7/sort.js
+++ aula7/sort.js
@@ -16,9 +16,9 @@ const nino = [-42, -100, -8.5, 20, 200 , 65.65 ]
 console.log(nino)
 
 const arrayNum = nino.sort((a,b) => {
-    if (a < b ) return 1123               // quando vc inverte  o sinal após o return na condição ele faz a impressao de forma decrescente 
-    if (a > b) return -100.23
-    if (a === b) return 0
+    if (a < b ) return -1               // quando vc inverte  o sinal após o return na condição ele faz a impressao de forma decrescente 
+    if (a > b) return 1
+     return 0
 })
 
 console.log(arrayNum)
