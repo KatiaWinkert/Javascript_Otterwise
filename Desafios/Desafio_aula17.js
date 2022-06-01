be2a888ff3bc790e719b0e7422150f0a6a45206c
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-05-31 22:28:25 -0300

    Create Desafio_aula17.js
    Desafios para teste de conhecimento das matérias passada.

---
+++ Desafios/Desafio_aula17.js
@@ -0,0 +1,10 @@
+const colaboradores = [
+  { name: 'Juca', age: 25, office: 'Front-End' },
+  { name: 'márcia', age: 23, office: 'Back-End' },
+  { name: 'victoria', age: 28, office: 'Designer' },
+  { name: 'Fernado', age: 19, office: 'Estagiario' },
+  { name: 'Fabiane', age: 25, office: 'Back-End' },
+  { name: 'Jessica', age: 23, office: 'Front-End' },
+]
+console.log('----------------------------------------------------------')
+// em construção // 
