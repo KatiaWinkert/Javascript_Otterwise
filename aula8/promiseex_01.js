531ff7d3604b4979bc33e74c896364abe803d264
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-06-16 13:48:26 -0300

    commit

--- aula8/promiseex_01.js
+++ aula8/promiseex_01.js
@@ -1,9 +1,6 @@
-/*const p1 = new Promise ((resolve, reje) => {
+const p1 = new Promise ((resolve, reje) => {
     resolve('sucesso')
 })
 
-console.log(p1)*/
+console.log(p1)
 
-setInterval(()=> {
-    console.log('Hello world')
-},0 )
