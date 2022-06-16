066034f8a292718130f0513ed05bda9830210aa4
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-06-16 15:38:15 -0300

    commit catch and finally
    homework

---
+++ aula8/promisecatch.js
@@ -0,0 +1,22 @@
+const value = 10 
+
+const p1 = new Promise((resolve, reject) => {
+  setInterval(() => {
+    if (typeof value === 'number'){
+        reject('Argumento não é um numero!')
+    }
+    resolve(value * 10 )
+  }, 2000)
+})
+
+p1.then(resp=> {
+    console.log(resp)
+  }).catch(error => {
+    console.log(error)
+}).finally(()=> {
+    console.log('Final')
+})
+
+
+// metodo catch ele é usado para quando se tem o reject assim como o .then é usado quando se te o resolve. 
+//finally executa o final 
