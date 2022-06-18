4dfe375579254ff2271aa18d30a40bc44dd73fd9
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-06-17 22:20:42 -0300

    commit exc02
    Destructuring assingnment - object

---
+++ aula9/exc02.js
@@ -0,0 +1,13 @@
+// Destructuring Assignment - object
+const user = {
+  nome: 'Katia',
+  idade: 33,
+}
+
+const { nome: username } = user
+console.log(username)
+
+// exemplos de como posso fazer o pedido da atribuição do  objeto
+// let name = user.nome
+// let age = user['idade']
+// na Desistruturação o nome da variavel pode ser igual o nome da propriedade. 
