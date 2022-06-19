ee7af96283f96222922172ebfbf0a684022030f8
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-06-18 23:06:10 -0300

    commit

--- aula9/exc03.js
+++ aula9/exc03.js
@@ -16,6 +16,8 @@ const user = {
 const { projects : projetos} = user 
 console.log(projetos)
 
-const p = [ {name: 'projeto 1'}, { name: 'projeto 2'}]
-const [p1, p2] = p
+const [p1] = projetos 
 console.log(p1) 
+
+const { name : nameprojetos } = p1 
+console.log(nameprojetos)
