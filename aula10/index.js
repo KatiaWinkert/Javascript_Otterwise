a4d24dfe590878bee9c1fee65dd4102fe209c1f7
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-06-24 23:41:14 -0300

    commit

--- aula10/index.js
+++ aula10/index.js
@@ -4,14 +4,12 @@ const result = formatterCurrency(20)
 
 console.log(result)*/ //=> Exemplo para um importação
 
-import { formatterCurrency  } from './module.js'
+//import { formatterCurrency  } from './module.js'
+import data from './data.json'
 
-const result = formatterCurrency(20)
-
+/*const result = formatterCurrency(20)
 console.log(result)
-
 import { formatteOp as fc , name } from './module.js' // => para auterar o nome tem que colocar (as)
-
 const nuvem = fc(20)
-
-console.log(nuvem)
+console.log(nuvem)*/
+console.log(data)
