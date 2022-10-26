e322f6b8026889031b7850bfddf51b04ce6e9242
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-10-26 08:45:58 -0300

    commit
    react

---
+++ react/aula01-videos/projeto-aula/src/App.js
@@ -0,0 +1,19 @@
+import textsection from './components/textsection'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > </a>
+      </header>
+      <main>
+        <textsection/>
+      </main>
+    </div>
+  )
+}
+export default App
---
+++ react/aula01-videos/projeto-aula/src/components/textsection.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function textsection() {
+  return (
+    <div>
+      <h1>Titulo</h1>
+      <p>
+        Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis,
+        bibendum egestas augue arcu ut est.Quem num gosta di mé, boa gentis num
+        é.Detraxit consequat et quo num tendi nada.Mauris nec dolor in eros
+        commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.
+      </p>
+    </div>
+  )
+}
+
+export default textsection
